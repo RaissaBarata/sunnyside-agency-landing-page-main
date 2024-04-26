8b4602a3fc5868252249bd5bdfc311ce84a813ac
RaissaBarata <barata.raissa@gmail.com>
2024-04-26 01:29:25 -0300

    correções

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const menuButton = document.querySelector(".menu-button");
 const mainNav = document.querySelector(".main-nav");
 
 menuButton.addEventListener("click", () => {
-    mainNav.classList.toggle("active")
-    console.log('clicou')
-})
+  mainNav.classList.toggle("active");
+});
